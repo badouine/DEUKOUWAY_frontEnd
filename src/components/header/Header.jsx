eb8bd5e57d4821295d15e1d32d151ed0160a3c31
badouine <nzbadouine@gmail.com>
2022-06-06 03:50:18 +0000

    card under

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,26 @@
 import {
   faBed,
+  faCalendarDays,
   faCar,
+  faPerson,
   faPlane,
   faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 const Header = () => {
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
   return (
     <div className="header">
       <div className="headerContainer">
@@ -39,6 +52,35 @@ const Header = () => {
           Contactez directement le proprietaire , et prenez vos clefs{" "}
         </p>
         <button className="headerBtn"> Enregistrement / Connexion </button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Fane gay dem ?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">Date</span>
+            <DateRange
+              editableDateInputs={true}
+              onChange={(item) => setDate([item.selection])}
+              moveRangeOnFirstSelection={false}
+              ranges={date}
+            />{" "}
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">
+              Adultes & enfants 1 chambre
+            </span>
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">Weureul ?</button>
+          </div>
+        </div>
       </div>
     </div>
   );
