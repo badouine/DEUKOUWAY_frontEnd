7ba8fbbfff8c91f3e26e8f2e58dacb1121e508bc
badouine <nzbadouine@gmail.com>
2022-06-11 04:33:19 +0000

    setOpenDate

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,45 +1,51 @@
-import './list.css';
+import "./list.css";
 import React from "react";
-import Navbar from '../../components/navbar/Navbar';
-import Header from '../../components/header/Header';
-import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import { format } from "date-fns";
+import { DateRange } from "react-date-range";
 
 const List = () => {
-
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
   const [date, setDate] = useState(location.state.date);
+  const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
 
   return (
     <div>
-      <Navbar/>
-      <Header type="list"/> 
+      <Navbar />
+      <Header type="list" />
       <div className="listContainer">
         <div className="listWrapper">
           <div className="listSearch">
             <h1 className="listTitle">Search</h1>
             <div className="listItem">
-              <label >Destination</label>
-              <input placeholder= {destination} type="text"/>
+              <label>Destination</label>
+              <input placeholder={destination} type="text" />
             </div>
             <div className="listItem">
-              <label >Check-in Date</label>
-              <span>
-              {`${format(date[0].startDate, "MM/dd/yyyy")} à ${format(
+              <label>Check-in Date</label>
+              <span onClick={() => setOpenDate(!openDate)}>
+                {`${format(date[0].startDate, "MM/dd/yyyy")} à ${format(
                   date[0].endDate,
                   "MM/dd/yyyy"
                 )}`}
               </span>
+             {openDate && <DateRange
+                onChange={(item) => setDate([item.selection])}
+                minDate={new Date()}
+                ranges={date}
+              />}
             </div>
           </div>
-            <div className="listResult"></div>
+          <div className="listResult"></div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
